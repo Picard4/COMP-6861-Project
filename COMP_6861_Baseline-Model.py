c2a7cdb12b497e5b658d1a7fbbfc25ee8670ca71
Palmarino DiMarco <60619342+Picard4@users.noreply.github.com>
2026-04-15 16:56:04 -0400

    Baseline model converted to a .py script

---
+++ COMP_6861_Baseline-Model.py
@@ -0,0 +1,263 @@
+import random
+import time
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from transformers import PreTrainedTokenizerFast
+
+# File constants
+data_file_path = "save-data/"
+tokenizer_file_path = data_file_path + "wikitext-tokenizer.json"
+train_data_file_path = data_file_path + "train-reduced-dataset-tokens.pt"
+valid_data_file_path = data_file_path + "valid-dataset-tokens.pt"
+test_data_file_path = data_file_path + "test-dataset-tokens.pt"
+baseline_file_path = data_file_path + "baseline-models/"
+
+def save_model_checkpoint(epoch, model, optimizer, scheduler, loss, best_valid_loss, save_file_name):
+    checkpoint = {
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "scheduler_state_dict": scheduler.state_dict(),
+        "loss": loss,
+        "best_valid_loss": best_valid_loss
+    }
+
+    torch.save(checkpoint, baseline_file_path + save_file_name)
+
+
+class WikitextDataset(Dataset):
+    def __init__(self, file_path, block_size):
+        # We need to start by loading data into CPU. We only move to GPU once batches are ready.
+        self.data = torch.load(file_path, mmap=True, weights_only=True).long()
+        self.block_size = block_size
+    
+    def __len__(self):
+        return len(self.data) - self.block_size
+    
+    def __getitem__(self, index):
+        chunk = self.data[index : index + self.block_size]
+        # The goal is to predict the next token, so we just need to shift our chunk by one.
+        target = self.data[index + 1 : index + self.block_size + 1]
+
+        return chunk, target
+
+class BaselineDecoderModel(nn.Module):
+    def __init__(self, tokenizer, block_size, d_key_value=64, nhead=6, n_layers=6, dropout=0.1, dim_feedforward_scalar=4):
+        super().__init__()
+
+        # It must be possible to divide d_model by nhead, so I figured it would be best to create the d_model within the __init__.
+        # Should make hyperparameter search a bit easier...
+        d_model = nhead * d_key_value
+
+        vocab_size = len(tokenizer)
+        self.padding_index = tokenizer.pad_token_id
+
+        self.token_embedding = nn.Embedding(vocab_size, d_model, padding_idx=self.padding_index)
+        self.positional_embedding = nn.Embedding(block_size, d_model, padding_idx=self.padding_index)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.transformer_blocks = nn.ModuleList([
+            nn.TransformerDecoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            dropout=dropout,
+            dim_feedforward=dim_feedforward_scalar * d_model,
+            batch_first=True,
+            norm_first=True
+            ) for layer in range(n_layers)
+        ])
+
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.linear_prediction_layer = nn.Linear(d_model, vocab_size)
+
+        self.criterion = nn.CrossEntropyLoss(ignore_index=self.padding_index, label_smoothing=0.1)
+        mask = torch.triu(torch.ones(block_size, block_size), diagonal=1).bool()
+        self.register_buffer('causal_mask', mask)
+
+    def forward(self, source_indices):
+        batch_size, sequence_length = source_indices.shape
+
+        # The causal_mask from the __init__ is designed to handle a mask at maximum capacity... which is rarely what we need in practice!
+        current_mask = self.causal_mask[:sequence_length, :sequence_length]
+
+        token_embeddings = self.token_embedding(source_indices)
+        positional_embeddings = self.positional_embedding(torch.arange(sequence_length, device=source_indices.device))
+
+        x = self.dropout(token_embeddings + positional_embeddings)
+
+        for block in self.transformer_blocks:
+            x = block(
+                tgt=x,
+                memory=x,
+                tgt_mask=current_mask,
+                memory_mask=current_mask,
+                tgt_is_causal=True,
+                memory_is_causal=True
+            )
+        
+        x = self.layer_norm(x)
+        logits = self.linear_prediction_layer(x)
+
+        return logits
+
+def train_epoch(model, dataloader, optimizer, scheduler, device, accumulation_steps):
+    model.train()
+    total_loss = 0.0
+
+    for i, batch in enumerate(dataloader):
+        full_sequence = batch[0].to(device)
+        x = full_sequence[:, :-1]
+        y = full_sequence[:, 1:]
+
+        # Use mixed-precision training to save some time!
+        with torch.amp.autocast(device_type=device.type, dtype=torch.bfloat16):
+            logits = model(x)
+
+            loss = model.criterion(
+                logits.reshape(-1, logits.size(-1)),
+                y.reshape(-1)
+            )
+
+            # Scale the loss based on the gradient accumulation
+            # The scaled loss affects our model, but we will use the unscaled loss to calculate the loss we want to return
+            scaled_loss = loss / accumulation_steps
+
+        scaled_loss.backward()
+        
+        # Use gradient accumulation to train more efficiently with less memory
+        if (i + 1) % accumulation_steps == 0 or (i + 1) == len(dataloader):
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad(set_to_none=True)
+
+        total_loss += loss.detach().item()
+
+        # Sleep every 10 batches to (hopefully) avoid microwaving my laptop
+        if (i + 1) % 10 == 0:
+            time.sleep(0.1)
+
+    return total_loss / len(dataloader)
+
+@torch.no_grad()
+def eval_model(
+    model,
+    dataloader,
+    device
+):
+    model.eval()
+    total_loss = 0.0
+
+    for i, batch in enumerate(dataloader):
+        full_sequence = batch[0].to(device)
+        x = full_sequence[:, :-1]
+        y = full_sequence[:, 1:]
+
+        # Use mixed-precision to save some time!
+        with torch.amp.autocast(device_type=device.type, dtype=torch.bfloat16):
+            logits = model(x)
+
+            loss = model.criterion(
+                logits.reshape(-1, logits.size(-1)),
+                y.reshape(-1)
+            )
+
+        total_loss += loss.detach().item()
+
+    return total_loss / len(dataloader)
+
+# This is the main code. Sets up the baseline model.
+def train_full_model(tokenizer):
+    # Setup before initializing the model
+    random.seed(0)
+    torch.manual_seed(0)
+
+    batch_size = 16
+    block_size = 256
+    accumulation_steps = 8
+
+    num_epochs = 10
+    warmup_pct_start = 0.1
+
+    train_dataset = WikitextDataset(train_data_file_path, block_size)
+    valid_dataset = WikitextDataset(valid_data_file_path, block_size)
+    test_dataset = WikitextDataset(test_data_file_path, block_size)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=4,
+        persistent_workers=True
+    )
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size
+    )
+
+    # Model initialization
+    model = BaselineDecoderModel(tokenizer, block_size)
+    model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=0.01)
+
+    # Due to gradient accumulation, we don't call optimizer.step on every loop.
+    # This is how many times we will *actually* call optimizer.step.
+    total_effective_steps = num_epochs * ((len(train_loader) + accumulation_steps - 1) // accumulation_steps)
+
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=5e-4,
+        total_steps=total_effective_steps,
+        pct_start=warmup_pct_start
+    )
+
+    # Model training
+    training_losses = []
+    valid_losses = []
+    best_valid_loss = float('inf')
+    try:
+        for epoch in range(1, num_epochs + 1):
+            epoch_training_loss = train_epoch(model, train_loader, optimizer, scheduler, device, accumulation_steps)
+            epoch_valid_loss = eval_model(model, valid_loader, device)
+            print(f"Epoch {epoch} | Training loss: {epoch_training_loss} | Valid loss: {epoch_valid_loss}")
+
+            training_losses.append(epoch_training_loss)
+            valid_losses.append(epoch_valid_loss)
+            if (epoch_valid_loss < best_valid_loss):
+                best_valid_loss = epoch_valid_loss
+                save_model_checkpoint(epoch, model, optimizer, scheduler, epoch_training_loss, best_valid_loss, f"BestModel.pt")
+            else:
+                save_model_checkpoint(epoch, model, optimizer, scheduler, epoch_training_loss, best_valid_loss, f"Checkpoint.pt")
+
+        test_loss = eval_model(model, test_loader, device)
+        print(f"Final Test Loss: {test_loss}")
+        torch.save({"training_losses": training_losses, "valid_losses": valid_losses, "test_loss": test_loss}, baseline_file_path + f"Losses.pt")
+    except:
+        if training_losses:
+            save_model_checkpoint(epoch, model, optimizer, scheduler, epoch_training_loss, best_valid_loss, f"Crash-Checkpoint.pt")
+            torch.save({"training_losses": training_losses, "valid_losses": valid_losses}, baseline_file_path + f"Losses.pt")
+    finally:
+        torch.cuda.empty_cache()
+
+if __name__ == "__main__":
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
+
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_file_path)
+
+    # We need to ensure the tokenizer knows what the special tokens mean.
+    tokenizer.add_special_tokens({
+        "pad_token": "<pad>",
+        "unk_token": "<unk>",
+        "eos_token": "<eos>",
+        "mask_token": "<mask>"
+    })
+
+    train_full_model(tokenizer)
