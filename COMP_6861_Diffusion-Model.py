60a93dcfbc948bbe0d11a712f645ba98083d9e40
Palmarino DiMarco <60619342+Picard4@users.noreply.github.com>
2026-04-24 02:59:34 -0400

    train epoch and eval model remain...

--- COMP_6861_Diffusion-Model.py
+++ COMP_6861_Diffusion-Model.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import time
+import math
 import torch
 import torch.nn as nn
 import optuna
@@ -10,6 +11,9 @@ from utils import WikitextDataset, save_model_checkpoint, get_device, get_reduce
 from utils import TRAIN_DATA_FILE_PATH, VALID_DATA_FILE_PATH, TEST_DATA_FILE_PATH, DIFFUSION_FILE_PATH, SUBSET_RATIO_OF_DATASET_TO_TRAIN, SUBSET_RATIO_OF_DATASET_TO_TUNE
 from utils import EARLY_STOP_EPOCHS, DIFFUSION_MODE_INDICATOR, BLOCK_SIZE_DIFFUSION, BATCH_SIZE_DIFFUSION, ACCUMULATION_STEPS_DIFFUSION
 
+LINEAR_NOISE_SCHEDULE = "linear"
+COSINE_NOISE_SCHEDULE = "cosine"
+
 def get_args():
     parser = argparse.ArgumentParser()
     
@@ -17,20 +21,26 @@ def get_args():
     
     return parser.parse_args()
 
+def get_noise_schedule(max_timesteps, schedule_type):
+    if schedule_type == LINEAR_NOISE_SCHEDULE:
+        return torch.linspace(0, 1, max_timesteps)
+    elif schedule_type == COSINE_NOISE_SCHEDULE:
+        steps = torch.arange(max_timesteps)
+        return 1.0 - torch.cos((steps / max_timesteps) * (math.pi / 2))
+
 # A noise_ratio of 0.7 mean we swap 70% of the tokens
-def forward_noise_process(tokens, vocab_size, noise_ratio, device):
+def forward_noise_process(tokens, vocab_size, noise_ratio, padding_index, device):
     random_values = torch.rand(tokens.shape, device=device)
-    mask = random_values < noise_ratio
+    mask = (random_values < noise_ratio) & (tokens != padding_index)
     random_tokens = torch.randint(0, vocab_size, tokens.shape, device=device)
     forward_noise_tokens = torch.where(mask, random_tokens, tokens)
     return forward_noise_tokens
 
 class DiffusionModel(nn.Module):
-    def __init__(self, tokenizer, block_size, nhead, nhead_scalar, num_layers, time_embedding_dim, dropout, dim_feedforward_scalar, label_smoothing, max_timesteps=1000, self_conditioning_prob=0.5):
+    def __init__(self, tokenizer, block_size, nhead, nhead_scalar, num_layers, time_embedding_dim, dropout, dim_feedforward_scalar, label_smoothing, max_timesteps):
         super().__init__()
 
         self.max_timesteps = max_timesteps
-        self.self_conditioning_prob = self_conditioning_prob
 
         # It must be possible to divide d_model by nhead, so I figured it would be best to create the d_model within the __init__.
         # Should make hyperparameter search a bit easier...
@@ -49,28 +59,62 @@ class DiffusionModel(nn.Module):
             nn.Linear(time_embedding_dim, d_model)
         )
 
-        encoder_layer = nn.TransformerEncoderLayer(
+        self.transformer_blocks = nn.ModuleList([
+            nn.TransformerEncoderLayer(
             d_model=d_model,
             nhead=nhead,
-            dim_feedforward = dim_feedforward_scalar * d_model,
+            dim_feedforward=dim_feedforward_scalar * d_model,
             dropout=dropout,
             batch_first=True,
             norm_first=True
-        )
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+            ) for layer in range(num_layers)
+        ])
+
+        # These blocks ensure that the time information attached to the Transformer block input is up-to-date
+        self.time_projection_blocks = nn.ModuleList([
+            nn.Linear(d_model, d_model) for layer in range(num_layers)
+        ])
+
         self.layer_norm = nn.LayerNorm(d_model)
         self.linear_head = nn.Linear(d_model, vocab_size)
 
         self.criterion = nn.CrossEntropyLoss(ignore_index=padding_index, label_smoothing=label_smoothing)
+        self._initialize_weights()
 
-    def forward(self, source_indices):
+    def forward(self, source_indices, time_steps):
         batch_size, sequence_length = source_indices.shape
 
-        
+        token_embeddings = self.token_embedding(source_indices)
+        positional_embeddings = self.positional_embedding(torch.arange(sequence_length, device=source_indices.device)).unsqueeze(0)
+        time_information = self.time_embedding(time_steps.float().view(-1, 1) / self.max_timesteps).unsqueeze(1)
+
+        print(token_embeddings.shape, flush=True)
+        print(positional_embeddings.shape, flush=True)
+        print(time_information.shape, flush=True)
+
+        x = token_embeddings + positional_embeddings
+        for i, block in enumerate(self.transformer_blocks):
+            # Inject the time information for each block to keep the model aware of how many steps it has.
+            x = block(x + self.time_projection_blocks[i](time_information))
+        x = self.layer_norm(x)
+        logits = self.linear_head(x)
 
         return logits
+    
+    def _initialize_weights(self):
+        # This initialization helps the model learn language before it needs to learn diffusion.
+        # Taken from: https://openreview.net/forum?id=E4roJSM9RM
 
-def train_epoch(model, vocab_size, dataloader, optimizer, scheduler, accumulation_steps, device):
+        # Initialize the last layer of the time embedding to zero.
+        nn.init.zeros_(self.time_embedding[-1].weight)
+        nn.init.zeros_(self.time_embedding[-1].bias)
+
+        # Initialize every time projection layer to zero.
+        for projection in self.time_projection_blocks:
+            nn.init.zeros_(projection.weight)
+            nn.init.zeros_(projection.bias)
+
+def train_epoch(model, vocab_size, dataloader, optimizer, scheduler, accumulation_steps, noise_schedule, device):
     model.train()
     total_loss = 0.0
 
@@ -118,6 +162,7 @@ def train_epoch(model, vocab_size, dataloader, optimizer, scheduler, accumulatio
 def eval_model(
     model,
     dataloader,
+    noise_schedule,
     device
 ):
     model.eval()
@@ -143,13 +188,14 @@ def eval_model(
 # Datasets are assumed to use the same block_size as the datasets that are sent in.
 # Send in None for the test_dataset to indicate that this training is for hyperparameter tuning.
 def train_full_model(tokenizer, train_dataset, valid_dataset, test_dataset, device, num_epochs=10,
-                    n_layers=6, d_key_value=64, nhead=6, dim_feedforward_scalar=4, lr=5e-4,    # L1 Hyperparameters
-                    warmup_pct_start=0.1, dropout=0.1, weight_decay=0.01, label_smoothing=0.1, # L2 Hyperparameters
+                    lr=5e-4, max_timesteps=1000,                                                # Hyperparameters that will not be in the search
+                    nhead=6, nhead_scalar=48, num_layers=6, time_embedding_dim=96,              # L1 Hyperparameters
+                    dim_feedforward_scalar=4, noise_schedule_type=COSINE_NOISE_SCHEDULE,        # L1 Hyperparameters
+                    warmup_pct_start=0.1, dropout=0.1, weight_decay=0.05, label_smoothing=0.1,  # L2 Hyperparameters (if time allows)
                     trial=None):  # The trial for tuning.
     # Setup before initializing the model
     start_time = time.time()
     set_randomization_seed()
-    vocab_size = len(tokenizer)
 
     train_loader = DataLoader(
         train_dataset,
@@ -172,22 +218,24 @@ def train_full_model(tokenizer, train_dataset, valid_dataset, test_dataset, devi
 
     # Model initialization
     model = DiffusionModel(tokenizer, BLOCK_SIZE_DIFFUSION, 
-                                 d_key_value, nhead, n_layers, dropout, dim_feedforward_scalar,
-                                 label_smoothing)
+                                 nhead, nhead_scalar, num_layers, time_embedding_dim, 
+                                 dropout, dim_feedforward_scalar, label_smoothing, max_timesteps)
     model.to(device)
 
     optimizer = get_optimizer(model.parameters(), lr, weight_decay)
     scheduler = get_scheduler(optimizer, lr, num_epochs, train_loader, ACCUMULATION_STEPS_DIFFUSION, warmup_pct_start)
+    noise_schedule = get_noise_schedule(max_timesteps, noise_schedule_type)
 
     # Model training
     training_losses = []
     valid_losses = []
     best_valid_loss = float('inf')
     no_improvement_epochs = 0
+    vocab_size = len(tokenizer)
     try:
         for epoch in range(1, num_epochs + 1):
-            epoch_training_loss = train_epoch(model, vocab_size, train_loader, optimizer, scheduler, ACCUMULATION_STEPS_DIFFUSION, device)
-            epoch_valid_loss = eval_model(model, valid_loader, device)
+            epoch_training_loss = train_epoch(model, vocab_size, train_loader, optimizer, scheduler, ACCUMULATION_STEPS_DIFFUSION, noise_schedule, device)
+            epoch_valid_loss = eval_model(model, valid_loader, noise_schedule, device)
             print(f"Epoch {epoch} | Training loss: {epoch_training_loss} | Valid loss: {epoch_valid_loss}", flush=True)
 
             training_losses.append(epoch_training_loss)
@@ -209,7 +257,7 @@ def train_full_model(tokenizer, train_dataset, valid_dataset, test_dataset, devi
                 if trial.should_prune():
                     raise optuna.TrialPruned()
         if test_dataset:
-            test_loss = eval_model(model, test_loader, device)
+            test_loss = eval_model(model, test_loader, noise_schedule, device)
             print(f"Final Test Loss: {test_loss}", flush=True)
             end_time = time.time()
             total_duration = end_time - start_time
@@ -228,10 +276,37 @@ def train_full_model(tokenizer, train_dataset, valid_dataset, test_dataset, devi
     return best_valid_loss
 
 def hyperparameter_tuning_objective_l1(trial, tokenizer, num_epochs, train_dataset, valid_dataset, device):
-    pass
+    nhead = trial.suggest_categorical("nhead", [4, 8, 12])
+    nhead_scalar = trial.suggest_int("nhead_scalar", 32, 64)
+    num_layers = trial.suggest_int("n_layers", 4, 12)
+    time_embedding_dim = trial.suggest_int("time_embedding_dim", 64, 128)
+    dim_feedforward_scalar = trial.suggest_int("dim_feedforward_scalar", 2, 4)
+    noise_schedule_type = trial.suggest_categorical("noise_schedule_type", [LINEAR_NOISE_SCHEDULE, COSINE_NOISE_SCHEDULE])
+
+    # These two hyperparameters make up the d_model.
+    # If it's too high, my training will crash!
+    if (nhead * nhead_scalar) > 768:
+        raise optuna.exceptions.TrialPruned()
+
+    validation_loss = train_full_model(tokenizer, train_dataset, valid_dataset, None, device, num_epochs,
+                                       lr=5e-4, max_timesteps=1000,
+                                       nhead=nhead, nhead_scalar=nhead_scalar, num_layers=num_layers, time_embedding_dim=time_embedding_dim, dim_feedforward_scalar=dim_feedforward_scalar, noise_schedule_type=noise_schedule_type,
+                                       warmup_pct_start=0.1, dropout=0.1, weight_decay=0.05, label_smoothing=0.1,
+                                       trial=trial)
+    return validation_loss
 
 def hyperparameter_tuning_objective_l2(trial, tokenizer, num_epochs, train_dataset, valid_dataset, device):
-    pass
+    warmup_pct_start = trial.suggest_float("warmup_pct_start", 0.05, 0.3)
+    dropout = trial.suggest_float("dropout", 0.05, 0.3)
+    weight_decay = trial.suggest_float("weight_decay", 1e-4, 0.3, log=True)
+    label_smoothing = trial.suggest_float("label_smoothing", 0.0, 0.2)
+
+    validation_loss = train_full_model(tokenizer, train_dataset, valid_dataset, None, device, num_epochs,
+                                       lr=5e-4, max_timesteps=1000,
+                                       nhead=6, nhead_scalar=48, num_layers=6, time_embedding_dim=96, dim_feedforward_scalar=4, noise_schedule_type=COSINE_NOISE_SCHEDULE,
+                                       warmup_pct_start=warmup_pct_start, dropout=dropout, weight_decay=weight_decay, label_smoothing=label_smoothing,
+                                       trial=trial)
+    return validation_loss
 
 if __name__ == "__main__":
     device = get_device()
@@ -261,7 +336,7 @@ if __name__ == "__main__":
         if args.tune == 1:
             study.optimize(
                 lambda trial: hyperparameter_tuning_objective_l1(trial, tokenizer, num_epochs, train_dataset, valid_dataset, device), 
-                n_trials=12
+                n_trials=10
             )
         else:
             study.optimize(
@@ -272,5 +347,6 @@ if __name__ == "__main__":
     else:
         print("Training model...", flush=True)
         test_dataset = WikitextDataset(TEST_DATA_FILE_PATH, BLOCK_SIZE_DIFFUSION, mode=DIFFUSION_MODE_INDICATOR)
-        train_full_model(tokenizer, train_dataset, valid_dataset, test_dataset, device, 10
+        train_full_model(tokenizer, train_dataset, valid_dataset, test_dataset, device, 10, 
+                         lr=5e-4, max_timesteps=1000
                          )
